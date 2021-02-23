6ee3846ff21b0725a4b557dcb6bb916d57b60252
roodrigo <roodrigoroot@gmail.com>
2021-02-23 17:26:24 -0600

    Changes to html

--- companies/urls.py
+++ companies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CompanyListCreateAPIView, CompanyRetrieveUpdateDestroyAPIView
+from .views import CompanyListCreateAPIView, CompanyRetrieveUpdateDestroyAPIView,CompanyTemplateView
 
 urlpatterns = [
     path("api/v1/companies/", CompanyListCreateAPIView.as_view(), name="create_company"),
     path("api/v1/companies/<uuid:auto_id>/", CompanyRetrieveUpdateDestroyAPIView.as_view(), name="rud_company"),
-    
+    path('', CompanyTemplateView.as_view(), name="index")
 ]
